e0c878e2623078ff42453128c99cad33d0e08797
Mansur <mansur.ham@mail.ru>
2022-07-16 22:56:01 +0300

    stilled pep8

--- blog/__init__.py
+++ blog/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from config import Config
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
@@ -10,3 +11,5 @@ app.config.from_object(Config)
 migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+
+from blog import routes, models
